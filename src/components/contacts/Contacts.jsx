98ed0d90ba314860bd17022f758a0ebb0bcaacd0
Nicolas <nick2003@email.cz>
2023-04-08 23:19:40 +0200

    Pro dnesek koncim

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -31,10 +31,10 @@ const Contacts = () => {
           </div>
           <div className="contacts">
             <h1 className='contacts-title'>Kontakt na mě</h1>
-            <a className='location' href="https://www.google.com/maps/place/Výživové+Poradenstv%C3%AD+Lucie/@49.9520795,16.1630052,17z/data=!3m1!4b1!4m6!3m5!1s0x470dc1192e940bc3:0x5ea83f73f67c527!8m2!3d49.9520795!4d16.1651939!16s%2Fg%2F11hndk7y2r"><span><ImLocation2/><span className='text-aside'>Nutriční poradenství Lucie, Žerotínova 438, Vysoké Mýto</span></span></a>
-            <a className='telephone' href="tel:725100006"><span><BsFillTelephoneInboundFill/><span className='text-aside'> 725 100 006</span></span></a>
-            <a className='location' href="mailto:nutricni.poradenstvi.vm@seznam.cz"><span><IoIosMail/><span className='text-aside'>nutricni.poradenstvi.vm@seznam.cz</span></span></a>
-            <a className='instagram' href="www.instagram.com/nutricniporadenstvi.lucie/" target={'_blank'}><span><RiInstagramFill/><span className='text-aside'>nutricniporadenstvilucie</span></span></a>
+            <a className='location' href="https://www.google.com/maps/place/Výživové+Poradenstv%C3%AD+Lucie/@49.9520795,16.1630052,17z/data=!3m1!4b1!4m6!3m5!1s0x470dc1192e940bc3:0x5ea83f73f67c527!8m2!3d49.9520795!4d16.1651939!16s%2Fg%2F11hndk7y2r"><span className='contact-icon'><ImLocation2/><span className='text-aside'>Nutriční poradenství Lucie, Žerotínova 438, Vysoké Mýto</span></span></a>
+            <a className='telephone' href="tel:725100006"><span className='contact-icon'><BsFillTelephoneInboundFill/><span className='text-aside'> 725 100 006</span></span></a>
+            <a className='location' href="mailto:nutricni.poradenstvi.vm@seznam.cz"><span className='contact-icon'><IoIosMail/><span className='text-aside'>nutricni.poradenstvi.vm@seznam.cz</span></span></a>
+            <a className='instagram' href="https://www.instagram.com/nutricniporadenstvi.lucie/" rel='noreferrer' target={'_blank'}><span className='contact-icon'><RiInstagramFill/><span className='text-aside'>nutricniporadenstvilucie</span></span></a>
           </div>
         </div>
       </div>
